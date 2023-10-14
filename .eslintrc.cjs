6baf0d6bac41df8b7b02c1a92a3dd187d214a8df
Joona Loukkola <111729194+siikabb@users.noreply.github.com>
2023-10-14 12:11:20 +0300

    fix linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: 'standard-with-typescript',
+  extends: ['google', 'eslint:recommended', 'plugin:prettier/recommended'],
   overrides: [
     {
       env: {
