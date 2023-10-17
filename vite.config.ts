89a03619889f3b6bd22443ec73073cc8900af899
Joona Loukkola <111729194+siikabb@users.noreply.github.com>
2023-10-17 16:30:13 +0300

    implement pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,50 @@
 import {defineConfig} from 'vite';
+import {VitePWA} from 'vite-plugin-pwa';
 
 export default defineConfig({
   build: {
     target: 'esnext',
   },
   base: './',
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,jpg,svg}'],
+      },
+      manifest: {
+        name: 'LunchApp',
+        short_name: 'LunchApp',
+        description: 'Opiskelijaravintolasovellus',
+        theme_color: '#ebd03b',
+        icons: [
+          {
+            src: 'src/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'src/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'src/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'src/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
 });
